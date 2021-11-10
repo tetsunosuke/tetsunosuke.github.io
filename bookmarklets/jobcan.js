663599d4e296feea2ae427bcfda9498f170e08f5
ITO Tetsunosuke <tetsunosuke.ito@photocreate.co.jp>
2021-11-10 14:39:00 +0900

    アルバイト賞与に関する処理を修正

--- bookmarklets/jobcan.js
+++ bookmarklets/jobcan.js
@@ -1,5 +1,6 @@
 const year = new Date().getYear() + 1900 + "";
-const isPhotocreate = document.querySelector("[href='/clients/offices']").innerText === "株式会社フォトクリエイト";
+const officeSelector =`[href="/clients/offices"]`;
+const isPhotocreate = document.querySelector(officeSelector).innerText === "株式会社フォトクリエイト";
 const div = document.createElement("div");
 div.style="width:80%;z-index:9999;text-align:center;position:absolute;left:100;top:100;background-color:#FFF;margin:50px;padding:50px;";
 div.innerHTML= "<p>読込中</p>";
@@ -25,6 +26,13 @@ const formula = json.values.all_payrolls.filter(p => p.payment_date.substring(0,
         return `${p.label} (総支給額: (${p.payment_amount}) - 非課税通勤手当(${p.payment_amount-p.income_tax_related_amount_exclude_no_exemption_commuting})) ✕ 3 = ${p.income_tax_related_amount_exclude_no_exemption_commuting*3}`;
     }
 }).join("\n");
+const innerHtml = `<h1>トータル金額：${total}</h1>
+<hr/>
+<p>計算式です</p>
+<div>
+<textarea rows="15" cols="80">${formula}</textarea>
+</div>`;
+div.innerHTML = innerHtml;
 
 // 賞与データ
 fetch("/employees/my_data/bonus").then((response) => response.text()).then((html) => {
@@ -32,35 +40,23 @@ fetch("/employees/my_data/bonus").then((response) => response.text()).then((html
     const doc = domParser.parseFromString(html, "text/html");
     const props = doc.querySelector("div").getAttribute("data-react-props");
 
-    
     const json = JSON.parse(props);
     if (json.error_status === "not_found") {
-        const innerHtml = `<h1>トータル金額：${total}</h1>
-        <hr/>
-        <p>計算式です</p>
-        <div>
-        <textarea rows="15" cols="80">${formula}</textarea>
-        </div>`;
-        div.innerHTML = innerHtml;
         return;
     }
-    // label, pay_on, total_taxable_target_amount
     // 合計金額算出
     const bonus = json.values.all_bonus.filter( o => o.pay_on.substring(0, 4) === year);
     if (bonus.length === 0) {
-        const innerHtml = `<h1>トータル金額：${total}</h1>
-        <hr/>
-        <p>計算式です</p>
-        <div>
-        <textarea rows="15" cols="80">${formula}</textarea>
-        </div>`;
-        div.innerHTML = innerHtml;
         return;
     }
     // 11月賞与の見込み（フォトクリのみ)は4月分を反映
-    //const nov_bonus = (isPhotocreate ? json.values.all_bonus.filter( o => o.pay_on.substring(0,7) === year + "-04")[0].total_taxable_target_amount : 0) -0;
-    const apr_bonus = json.values.all_bonus.filter( o => o.pay_on.substring(0,7) === year + "-04");
-    const nov_bonus = apr_bonus.length > 0 ? (isPhotocreate ? json.values.all_bonus.filter( o => o.pay_on.substring(0,7) === year + "-04")[0].total_taxable_target_amount : 0) -0: 0;
+    let apr_bonus = json.values.all_bonus.filter( o => o.pay_on.substring(0,7) === year + "-04");
+    let apr_bonus_value = 0;
+    if (apr_bonus.length > 0) {
+        apr_bonus_value = apr_bonus[0];
+    }
+
+    const nov_bonus = apr_bonus_value > 0 ? (isPhotocreate ? json.values.all_bonus.filter( o => o.pay_on.substring(0,7) === year + "-04")[0].total_taxable_target_amount : 0) -0: 0;
     const bonusTotal = bonus.reduce( (acc, cv) => { acc.push(cv.total_taxable_target_amount); return acc; },[]).reduce( (acc, cv) => acc+cv, 0) + nov_bonus;
     const bonusFormula = bonus.map( (o) => {
         if (nov_bonus > 0 && o.pay_on.substring(0,7) === year + "-04") {
@@ -69,13 +65,13 @@ fetch("/employees/my_data/bonus").then((response) => response.text()).then((html
             return `${o.label.trim()} = ${o.total_taxable_target_amount}`
         }
     }).join("\n");
-     // 新しい要素を取得
+
     const innerHtml = `<h1>トータル金額：${total+bonusTotal} (${total} + ${bonusTotal})</h1>
     <hr/>
     <p>計算式です</p>
     <div>
     <textarea rows="15" cols="80">${formula}\n${bonusFormula}</textarea>
     </div>`;
-     div.innerHTML = innerHtml;
+    div.innerHTML = innerHtml;
 });
 
